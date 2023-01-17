bad05758d25ef25538b3693e898b45391804db15
Samara <samcarvalhos@hotmail.com>
2023-01-17 14:12:47 -0300

    [chore] - reset services test

--- web/shop/pages/index.tsx
+++ web/shop/pages/index.tsx
@@ -2,7 +2,6 @@ import { defaultSeo } from 'src/config/seo'
 import { useConfig } from '@contexts/ConfigContext'
 import { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
-import { api } from '@hytzenshop/services'
 
 import HeaderFooterLayout from '@layouts/HeaderFooterLayout'
 import MainProductsList from '@features/home/MainProductsList'
@@ -13,12 +12,6 @@ import React from 'react'
 
 const Home: NextPage = () => {
   const { announcement } = useConfig()
-
-  React.useEffect(() => {
-    fetch('https://shop.hytzen.com:4000/api')
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-  }, [])
 
   return (
     <HeaderFooterLayout glassEffect>
