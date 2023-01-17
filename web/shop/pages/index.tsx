c9119e64f7b8ed26beea2d80e5c98b0a3516e909
Samara <samcarvalhos@hotmail.com>
2023-01-17 09:07:42 -0300

    [test] - service monorepo

--- web/shop/pages/index.tsx
+++ web/shop/pages/index.tsx
@@ -2,6 +2,7 @@ import { defaultSeo } from 'src/config/seo'
 import { useConfig } from '@contexts/ConfigContext'
 import { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
+import { api } from '@hytzenshop/services'
 
 import HeaderFooterLayout from '@layouts/HeaderFooterLayout'
 import MainProductsList from '@features/home/MainProductsList'
@@ -12,6 +13,16 @@ import React from 'react'
 
 const Home: NextPage = () => {
   const { announcement } = useConfig()
+
+  const getMessage = async () => {
+    const message = await api.get('http://localhost:2090/api')
+
+    console.log(message.data)
+  }
+
+  React.useEffect(() => {
+    getMessage()
+  }, [])
 
   return (
     <HeaderFooterLayout glassEffect>
